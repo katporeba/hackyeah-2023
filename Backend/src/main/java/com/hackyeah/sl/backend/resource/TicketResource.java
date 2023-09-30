2e6c3b72836feffc2c7c3f280b0b3989e4625d87
bartek <smalecbartlomiej@gmail.com>
2023-09-30 22:54:57 +0200

    Implemented upload ticket photo

--- Backend/src/main/java/com/hackyeah/sl/backend/resource/TicketResource.java
+++ Backend/src/main/java/com/hackyeah/sl/backend/resource/TicketResource.java
@@ -5,21 +5,31 @@ import com.hackyeah.sl.backend.domain.DTO.TicketInOneKilometer;
 import com.hackyeah.sl.backend.domain.Ticket;
 import com.hackyeah.sl.backend.domain.mappers.TicketMapper;
 import com.hackyeah.sl.backend.service.TicketService;
+import com.hackyeah.sl.backend.service.UserServiceImpl;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.hackyeah.sl.backend.constant.FileConstant.FORWARD_SLASH;
+import static com.hackyeah.sl.backend.constant.FileConstant.USER_FOLDER;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping(path = {"/ticket"})
 public class TicketResource {
 
@@ -64,6 +74,25 @@ public class TicketResource {
     }
 
     @PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN','ROLE_ADMIN','ROLE_USER')")
+    @PostMapping("/add/image")
+    public ResponseEntity<Ticket> add(@RequestParam(name="tickedId") String ticketId ,@RequestParam(name = "file") MultipartFile file) throws IOException {
+
+        Ticket ticket = ticketService.getTicketsByTicketId(ticketId);
+
+        ticketService.saveTicketImage(ticket, file);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/image/{ticketId}/{filename}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getProfileImage(
+            @NotBlank @PathVariable String ticketId,
+            @NotBlank @PathVariable String filename)
+            throws IOException {
+        return Files.readAllBytes(Paths.get(USER_FOLDER + ticketId + FORWARD_SLASH + filename));
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_SUPER_ADMIN','ROLE_ADMIN','ROLE_USER')")
     @PostMapping("/list/nearby")
     public ResponseEntity<List<TicketDto>> findTicketsInOneKilometer(@RequestBody TicketInOneKilometer ticket) {
 
@@ -73,6 +102,8 @@ public class TicketResource {
 
         return new ResponseEntity<>(ticketDtos, HttpStatus.CREATED);
     }
+
+
 
 
 }
--- Backend/src/main/java/com/hackyeah/sl/backend/service/TicketService.java
+++ Backend/src/main/java/com/hackyeah/sl/backend/service/TicketService.java
@@ -2,7 +2,10 @@ package com.hackyeah.sl.backend.service;
 
 import com.hackyeah.sl.backend.domain.DTO.TicketDto;
 import com.hackyeah.sl.backend.domain.Ticket;
+import com.hackyeah.sl.backend.exception.domain.NotAnImageFileException;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface TicketService {
@@ -19,4 +22,10 @@ public interface TicketService {
     List<Ticket> getTicketsByEmail(String email);
 
     Ticket extendTicketTime(String ticketId);
+
+    Ticket getTicketsByTicketId(String ticketId);
+
+    void saveTicketImage(Ticket ticket, MultipartFile image) throws IOException, NotAnImageFileException;
+
+
 }
