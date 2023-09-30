e8bbfe251c9ec6c5357e9b1b677fb28727b1a140
Kacper <kpawlak2000@gmail.com>
2023-09-30 19:11:48 +0200

    sign in page

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -5,9 +5,11 @@ import CircularProgress from "@mui/material/CircularProgress";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { Link, Navigate, useNavigate } from "react-router-dom";
-import Header from "../../components/Header";
 import AuthService from "../../services/authService";
 import { isEmail, isPasswordStrong, markValidation } from "../../common/validateUtils/validateSignUp";
+import googleIcon from "../../assets/google_logo.png";
+import facebookIcon from "../../assets/facebook_logo.png";
+import appleIcon from "../../assets/apple_logo.png";
 
 export function SignIn() {
   const [post, setPost] = useState(null);
@@ -45,16 +47,16 @@ export function SignIn() {
   };
 
   return (
+    // add navigation to homepage
     <>
-      <Header selected="signUp" theme="light" />
+      <div className="navbox" />
       <Box className="signup-container">
-        <Box className="signup-content-left" />
         <Box className="signup-content-right">
-          <Typography className="header">Zaloguj się</Typography>
+          <Typography className="header">Logowanie</Typography>
           <Box className="signup-inputs">
             <Typography className="input-description">Email</Typography>
             <TextField
-              className={!validateEmail ? "error" : "success"}
+              className={`input-field ${!validateEmail ? "error" : "success"}`}
               id="email-input"
               placeholder="Podaj e-mail"
               variant="outlined"
@@ -64,10 +66,21 @@ export function SignIn() {
                 markValidation(isEmail(e.target.value), setValidateEmail);
                 onChangeInput(e.target.value, setEmail);
               }}
+              sx={{
+                "& fieldset": {
+                  borderRadius: "50px",
+                  border: "none",
+                },
+                input: {
+                  "&::placeholder": {
+                    padding: "0 .5rem",
+                  },
+                },
+              }}
             />
             <Typography className="input-description">Hasło</Typography>
             <TextField
-              className={!validatePassword ? "error" : "success"}
+              className={`input-field ${!validatePassword ? "error" : "success"}`}
               id="password-input"
               placeholder="Podaj hasło"
               variant="outlined"
@@ -91,22 +104,54 @@ export function SignIn() {
                   </InputAdornment>
                 ),
               }}
+              sx={{
+                "& fieldset": {
+                  borderRadius: "50px",
+                  border: "none",
+                },
+                input: {
+                  "&::placeholder": {
+                    padding: "0 .5rem",
+                  },
+                },
+              }}
             />
             {error && (
               <Typography className="input-description error-message">{error.response.data.message}</Typography>
             )}
             {post && <Navigate to="/statistics" replace />}
           </Box>
+          <Box className="divider" />
+          <Typography className="or-signin-with">Lub zaloguj się z</Typography>
+          <Box className="social-media-buttons">
+            <Button variant="outlined" className="social-media-button">
+              <img src={googleIcon} alt="google-icon" />
+            </Button>
+            <Button variant="outlined" className="social-media-button">
+              <img src={facebookIcon} alt="facebook-icon" />
+            </Button>
+            <Button variant="outlined" className="social-media-button">
+              <img src={appleIcon} alt="apple-icon" />
+            </Button>
+          </Box>
           <Box className="signup-buttons">
             {loading ? (
               <CircularProgress className="progress-circular" />
             ) : (
-              <Button onClick={() => onSubmitSendData()} disabled={!(validateEmail && validatePassword)}>
+              <Button
+                variant="contained"
+                onClick={() => onSubmitSendData()}
+                disabled={!(validateEmail && validatePassword)}
+                className="signup-button"
+              >
                 Zaloguj się
               </Button>
             )}
             <Typography className="switch-site">
-              Nie masz jeszcze konta? <Link to="/sign-up">Zarejestruj się</Link>
+              Nie masz jeszcze konta?{" "}
+              <Link className="switch-site-link" to="/sign-up">
+                Zarejestruj się
+              </Link>
             </Typography>
           </Box>
         </Box>
